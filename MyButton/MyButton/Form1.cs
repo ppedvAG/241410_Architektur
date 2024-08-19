3661618dc7896a3f61e5e3e9d2dc23706efb22bc
André <AndreR@ppedv.de>
2024-08-19 14:23:35 +0200

    Template pattern mit WinForms cutstom Button

---
+++ MyButton/MyButton/Form1.cs
@@ -0,0 +1,10 @@
+namespace MyButton
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MyButton/MyButton/MyButton.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing.Drawing2D;
+
+namespace MyButton
+{
+    internal class MyButton : Button
+    {
+        int c = 0;
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            c++;
+            //base.OnPaint(pevent);
+            pevent.Graphics.FillRectangle(new SolidBrush(Parent.BackColor), ClientRectangle);
+            pevent.Graphics.FillEllipse(new LinearGradientBrush(ClientRectangle,Color.Yellow,Color.WhiteSmoke,90f), ClientRectangle);
+            var sf = new StringFormat() { Alignment = StringAlignment.Center, LineAlignment = StringAlignment.Center };
+            pevent.Graphics.DrawString($"{c}x { Text}", SystemFonts.DefaultFont, Brushes.HotPink, ClientRectangle, sf);
+        }
+    }
+}
