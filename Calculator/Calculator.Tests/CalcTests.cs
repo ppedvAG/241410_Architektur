e53d1bf802792d9e17394c4f8bc42385f31d6db6
André <AndreR@ppedv.de>
2024-08-20 10:17:46 +0200

    Tests mit MS Fakes

--- Calculator/Calculator.Tests/CalcTests.cs
+++ Calculator/Calculator.Tests/CalcTests.cs
@@ -1,3 +1,5 @@
+using Microsoft.QualityTools.Testing.Fakes;
+
 namespace Calculator.Tests
 {
     public class CalcTests
@@ -82,6 +84,46 @@ namespace Calculator.Tests
                 Assert.True(calc.IsWeekend(dt));
             else
                 Assert.False(calc.IsWeekend(dt));
+        }
+
+        [Theory]
+        [InlineData(23, 12, false)]
+        [InlineData(24, 12, true)]
+        public void IsFeiertag(int day, int month, bool exp)
+        {
+            DateTime dt = new DateTime(2024, month, day);
+            Calc calc = new Calc();
+
+            Assert.Equal(exp, calc.IsFeiertag(dt));
+        }
+
+        [Fact]
+        public void IsWeekendOrFeiertagToday()
+        {
+            Calc calc = new Calc();
+            using (ShimsContext.Create())
+            {
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2024, 8, 19);
+                Assert.False(calc.IsWeekendOrFeiertagToday());//Mo
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2024, 8, 20);
+                Assert.False(calc.IsWeekendOrFeiertagToday());//Di
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2024, 8, 21);
+                Assert.False(calc.IsWeekendOrFeiertagToday());//Mi
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2024, 8, 22);
+                Assert.False(calc.IsWeekendOrFeiertagToday());//Do
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2024, 8, 23);
+                Assert.False(calc.IsWeekendOrFeiertagToday());//Fr
+
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2024, 8, 24);
+                Assert.True(calc.IsWeekendOrFeiertagToday());//Sa
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2024, 8, 25);
+                Assert.True(calc.IsWeekendOrFeiertagToday());//So
+
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2024, 12, 24);
+                Assert.True(calc.IsWeekendOrFeiertagToday());//24.12
+                System.Fakes.ShimDateTime.NowGet = () => new DateTime(2024, 12, 25);
+                Assert.True(calc.IsWeekendOrFeiertagToday());//25.12
+            }
         }
 
     }
--- Calculator/Calculator/Calc.cs
+++ Calculator/Calculator/Calc.cs
@@ -1,10 +1,11 @@
-﻿using System.Data;
+﻿using System.Runtime.CompilerServices;
+[assembly: InternalsVisibleTo("Calculator.Tests")]
 
 namespace Calculator
 {
     public class Calc
     {
-        public int Sum(int a, int b)
+        internal int Sum(int a, int b)
         {
             return checked(a + b);
         }
@@ -12,7 +13,28 @@ namespace Calculator
         public bool IsWeekend(DateTime dt)
         {
             return dt.DayOfWeek == DayOfWeek.Sunday ||
-                   dt.DayOfWeek == DayOfWeek.Saturday;
+                    dt.DayOfWeek == DayOfWeek.Saturday;
+        }
+
+        public bool IsFeiertag(DateTime dt)
+        {
+            if (dt.Day == 24 && dt.Month == 12)
+                return true;
+
+            if (dt.Day == 25 && dt.Month == 12)
+                return true;
+
+            if (dt.Day == 26 && dt.Month == 12)
+                return true;
+
+            return false;
+        }
+
+        public bool IsWeekendOrFeiertagToday()
+        {
+            var now = DateTime.Now;
+
+            return IsWeekend(now) || IsFeiertag(now);
         }
     }
 }
