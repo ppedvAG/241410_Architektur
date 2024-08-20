2702460e3b4e7d3b8f363406b84e40e7766c4c1c
André <AndreR@ppedv.de>
2024-08-20 16:27:20 +0200

    Test mit FluentAss und AutoFix

--- ppedv.PuecklerPalace/ppedv.PuecklerPalace.Data.Db.Tests/PuecklerContextTests.cs
+++ ppedv.PuecklerPalace/ppedv.PuecklerPalace.Data.Db.Tests/PuecklerContextTests.cs
@@ -1,7 +1,10 @@
-
-
+using AutoFixture;
+using AutoFixture.Kernel;
+using FluentAssertions;
 using Microsoft.EntityFrameworkCore;
 using ppedv.PuecklerPalace.Model;
+using System.Diagnostics;
+using System.Reflection;
 
 namespace ppedv.PuecklerPalace.Data.Db.Tests
 {
@@ -53,8 +56,10 @@ namespace ppedv.PuecklerPalace.Data.Db.Tests
                 var loaded = con.Find<Eissorte>(testEis.Id);
                 //var loaded = con.Eissorten.FirstOrDefault(x => x.Id == testEis.Id);
 
-                Assert.NotNull(loaded);
-                Assert.Equal(testEis.Name, loaded.Name);
+                //Assert.NotNull(loaded);
+                loaded.Should().NotBeNull();
+                //Assert.Equal(testEis.Name, loaded.Name);
+                loaded.Name.Should().Be(testEis.Name);
                 Assert.False(testEis == loaded);
                 Assert.NotEqual(testEis, loaded);
             }
@@ -78,7 +83,8 @@ namespace ppedv.PuecklerPalace.Data.Db.Tests
                 var loaded = con.Find<Eissorte>(testEis.Id);
                 loaded!.Name = newName;
                 var result = con.SaveChanges();
-                Assert.Equal(1, result);
+                //Assert.Equal(1, result);
+                result.Should().Be(1);
             }
 
             //opt. verify
@@ -128,6 +134,65 @@ namespace ppedv.PuecklerPalace.Data.Db.Tests
             con.Add(testBestell);
             Assert.Throws<DbUpdateException>(() => con.SaveChanges());
 
+        }
+
+        [Fact]
+        [Trait("Category", "System")]
+        public void Insert_Bestellung_with_AutoFixture()
+        {
+            var fix = new Fixture();
+            fix.Behaviors.Add(new OmitOnRecursionBehavior());
+            fix.Customizations.Add(new TypeRelay(typeof(EisElement), typeof(Eissorte)));
+            fix.Customizations.Add(new PropertyNameOmitter(nameof(Entity.Id)));
+            var best = fix.Create<Bestellung>();
+
+            using (var con = new PuecklerContext(conString))
+            {
+                con.Database.EnsureCreated();
+                con.Add(best);
+                var rows = con.SaveChanges();
+                Debug.WriteLine($"ROWS: {rows}");
+            }
+
+            using (var con = new PuecklerContext(conString))
+            {
+                var loaded = con.Find<Bestellung>(best.Id);//Lazy Loading 
+                //var loaded = con.Bestellungen.Include(x => x.Positionen)
+                //                             .ThenInclude(x => x.Element)
+                //                             .ThenInclude(x => x.Zutaten)
+                //                             .FirstOrDefault(x => x.Id == best.Id); //Eager Loading 
+                //explizit loading 
+                //var loaded = con.Find<Bestellung>(best.Id);
+                //con.Entry(loaded).Collection(x => x.Positionen).Load();
+                //foreach (var item in loaded.Positionen)
+                //{
+                //    con.Entry(item).Reference(x => x.Element).Load();
+                //    con.Entry(item.Element).Collection(x => x.Zutaten).Load();
+                //}
+
+                loaded.Should().BeEquivalentTo(best, x => x.IgnoringCyclicReferences());
+            }
+
+        }
+
+    }
+
+    internal class PropertyNameOmitter : ISpecimenBuilder
+    {
+        private readonly IEnumerable<string> names;
+
+        internal PropertyNameOmitter(params string[] names)
+        {
+            this.names = names;
+        }
+
+        public object Create(object request, ISpecimenContext context)
+        {
+            var propInfo = request as PropertyInfo;
+            if (propInfo != null && names.Contains(propInfo.Name))
+                return new OmitSpecimen();
+
+            return new NoSpecimen();
         }
     }
 }
--- ppedv.PuecklerPalace/ppedv.PuecklerPalace.Model/BestellPosition.cs
+++ ppedv.PuecklerPalace/ppedv.PuecklerPalace.Model/BestellPosition.cs
@@ -1,9 +1,12 @@
-﻿namespace ppedv.PuecklerPalace.Model
+﻿using System.Runtime;
+
+namespace ppedv.PuecklerPalace.Model
 {
     public class BestellPosition : Entity
     {
         public virtual required Bestellung Bestellung { get; set; }
-        public virtual ICollection<Topping> Toppings { get; set; } = new List<Topping>();
-        public virtual ICollection<Eissorte> Eissorten { get; set; } = new List<Eissorte>();
+
+        public int Amount { get; set; }
+        public virtual required EisElement Element { get; set; }
     }
 }
